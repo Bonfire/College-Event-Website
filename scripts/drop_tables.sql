f117f03c13d306046790b9777604d901bbfa5623
Tiger <tgsachse@gmail.com>
2018-09-23 14:49:42 -0400

    prelim finished table scripts

---
+++ scripts/drop_tables.sql
@@ -0,0 +1,17 @@
+/*
+Part of Zajedno.
+Written by Tiger Sachse.
+*/
+
+CONNECT 'jdbc:derby:main_table';
+
+DROP TABLE users;
+DROP TABLE universities;
+DROP TABLE organizations;
+DROP TABLE events;
+DROP TABLE pictures;
+DROP TABLE comments;
+DROP TABLE memberships;
+
+DISCONNECT;
+EXIT;
--- scripts/make_tables.sql
+++ scripts/make_tables.sql
@@ -4,33 +4,55 @@ Written by Tiger Sachse.
 */
 
 CONNECT 'jdbc:derby:main_table;create=true';
+
+CREATE TABLE users (
+    username VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(20),
+    permissions INT
+);
 CREATE TABLE universities (
-    name VARCHAR(200),
+    name VARCHAR(200) PRIMARY KEY,
     address VARCHAR(300),
     description VARCHAR(1000),
     student_count INT,
-    picture_id INT
+    picture_owner_id INT
 );
-CREATE TABLE pictures (
-    owner_id INT PRIMARY KEY,
-    filename VARCHAR(200)
+CREATE TABLE organizations (
+    name VARCHAR(200) PRIMARY KEY,
+    owner VARCHAR(50)
 );
 CREATE TABLE events (
-    name VARCHAR(200),
-    category VARCHAR(100),
+    name VARCHAR(200) NOT NULL,
     description VARCHAR(1000),
-    time INT,
-    date INT,
-    address VARCHAR(300),
-    contact_number INT,
-    contact_email VARCHAR(100),
+    category VARCHAR(100),
+    address VARCHAR(300) NOT NULL,
     publicity_level VARCHAR(100),
     organization_name VARCHAR(100),
+    event_time INT,
+    event_date INT,
+    contact_number INT,
+    contact_email VARCHAR(100),
+    comment_owner_id INT,
+    ratings_count INT,
+    ratings_average INT,
     PRIMARY KEY (name, address)
 );
-CREATE TABLE users (
-    username VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(20)
+CREATE TABLE pictures (
+    owner_id INT,
+    filename VARCHAR(200)
 );
+CREATE TABLE comments (
+    event_id INT,
+    username VARCHAR(50),
+    time INT,
+    text VARCHAR(300),
+    PRIMARY KEY (event_id, time)
+);
+CREATE TABLE memberships (
+    organization VARCHAR(200),
+    username VARCHAR(50),
+    PRIMARY KEY (username, organization)
+);
+
 DISCONNECT;
 EXIT;
