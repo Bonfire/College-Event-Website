8f0ab460202156f29fc2cbc293faadb1a73d87cd
Baran Barut <bonfire.hf@gmail.com>
2018-10-10 05:12:27 -0400

    Edited database inclusion, working on login functionality

--- source/web/database.inc.php
+++ source/web/database.inc.php
@@ -6,10 +6,9 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=website", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
 }
 catch(PDOException $e)
 {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Connection to database failed: " . $e->getMessage();
 }
 ?>
