103837730d63d0fcb97490c41a6a3948768fb097
Tiger <tgsachse@gmail.com>
2018-10-13 17:15:15 -0400

    add more functions to the operations script

--- operations.sh
+++ operations.sh
@@ -2,9 +2,12 @@
 # Written by Tiger Sachse.
 
 USER="root"
+SCRIPTS="scripts"
 SOURCE_PHP_DIR="source/web"
 DEST_PHP_DIR="/var/www/html"
 DATABASE_NAME="main_database"
+GENERATION_SCRIPT="generate_data.py"
+DUMMY_DATA_SCRIPT="add_sample_data.sql"
 
 # Update the WWW directory with the latest PHP.
 function update_www {
@@ -21,13 +24,33 @@ function run_sql {
     done
 }
 
+# Generate data and create an SQL script for the database.
+function generate_data {
+    python3 $SCRIPTS/$GENERATION_SCRIPT
+    mv $DUMMY_DATA_SCRIPT $SCRIPTS
+}
+
+# Destroy and then rebuild the database, plus add dummy data.
+function rebuild_database {
+    generate_data
+    run_sql $DROP_SCRIPT $MAKE_SCRIPT $SCRIPTS/$DUMMY_DATA_SCRIPT
+}
+
 # Main entry-point to this script.
 case "$1" in
     "--update-www")
         update_www
         ;;
 
+    "--generate-data")
+        generate_data
+        ;;
+
     "--sql")
         run_sql "${@:2}"
+        ;;
+
+    "--rebuild-database")
+        rebuild_database 
         ;;
 esac
--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,9 +1,14 @@
-""""""
+"""Generate an SQL script with random data for a mySQL database.
+
+Part of Zajedno.
+Written by Tiger Sachse.
+"""
 
 import random
 import string
 from faker import Faker
 
+# Constants.
 HEADER = (
     "/*\n"
     "Part of Zajedno.\n"
@@ -12,11 +17,14 @@ HEADER = (
     "USE main_database;\n"
     "CONNECT main_database;\n\n"
 )
-FOOTER = "\nQUIT"
 FAKER = Faker()
+FOOTER = "\nQUIT"
+USER_COUNT = 1000
+UNIVERSITY_COUNT = 100
+OUTPUT_PATH = "add_sample_data.sql"
 
 class User:
-    """"""
+    """Class that holds randomized data for a user entry in a database."""
     INSERT = (
         "INSERT INTO users "
         "(username, password, email, university_id, permission_level) "
@@ -24,7 +32,7 @@ class User:
     )
 
     def __init__(self, university_id):
-        """"""
+        """Generate random data for each field for this user."""
         profile = FAKER.simple_profile()
 
         self.email = profile["mail"]
@@ -32,12 +40,14 @@ class User:
         self.username = profile["username"]
         self.password = self.__generate_password()
         self.permission_level = random.choice(("normal", "admin", "superadmin"))
-        
+
 
     def __generate_password(self):
-        """"""
+        """Generate a randomized password with special characters and digits."""
         password = []
 
+        # Include a capital letter, a special character, two digits,
+        # and several lowercase letters in the password.
         password.append(random.choice(string.ascii_uppercase))
         password.extend(
             random.choices(
@@ -54,7 +64,7 @@ class User:
 
 
     def __str__(self):
-        """"""
+        """Return the SQL insertion command for this user."""
         return self.INSERT.format(
             self.username,
             self.password,
@@ -65,7 +75,7 @@ class User:
 
         
 class University:
-    """"""
+    """Class that holds randomized data for a university entry in a database."""
     INSERT = (
         "INSERT INTO universities "
         "(name, address, description) "
@@ -73,14 +83,14 @@ class University:
     )
 
     def __init__(self):
-        """"""
+        """Generate random data for this university's fields."""
         self.name = self.__generate_name()
         self.address = FAKER.address()
-        self.description = FAKER.sentence()#FAKER.text(max_nb_chars=1000)
+        self.description = FAKER.sentence()
 
     
     def __generate_name(self):
-        """"""
+        """Generate a random name with a special institutional format."""
         INSTITUTION_FORMATS = (
             "{0} University",
             "University of {0}",
@@ -93,7 +103,7 @@ class University:
 
 
     def __str__(self):
-        """"""
+        """Return the SQL insertion command for this university."""
         return self.INSERT.format(
             self.name,
             self.address,
@@ -102,15 +112,15 @@ class University:
 
 
 # Main entry point to script.
-universities = [University() for count in range(100)]
-users = [User(random.randrange(1, len(universities))) for count in range(100)]
+# Generate random users and universities.
+universities = [University() for count in range(UNIVERSITY_COUNT)]
+users = [User(random.randrange(1, len(universities))) for count in range(USER_COUNT)]
 
-with open("test.sql", "w") as f:
+# Write all insertion commands, as well as a header and footer, to an SQL script.
+with open(OUTPUT_PATH, "w") as f:
     f.write(HEADER)
     for university in universities:
         f.write(str(university))
-
     for user in users:
         f.write(str(user))
-    
     f.write(FOOTER)
