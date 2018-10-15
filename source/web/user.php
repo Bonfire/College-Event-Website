f1426b95557015a3bbb7fd0856e1d0476b083bdf
Baran Barut <bonfire.hf@gmail.com>
2018-10-14 22:49:49 -0400

    Slight tweak in database.inc, added beginning API login functionality

---
+++ source/web/user.php
@@ -0,0 +1,58 @@
+<?php
+
+    include('database.inc.php');
+
+    // Get action from GET, and data from POST
+    $action = $_GET['action'];
+    $data = file_get_contents('php://input');
+
+    // See if the request has set an action
+    if(!isset($action))
+    {
+        // Kill the script if no action is set
+        echo "No action set";
+        die();
+    }
+
+    // Check database connection
+    if(!$conn)
+    {
+        echo "Error connecting to the database";
+        die();
+    }
+
+    if($action === "login")
+    {
+        $email = $data['inputEmail'];
+        $password = $data['inputPassword'];
+
+        if($query = $conn->prepare('SELECT id, username, permission_level  FROM users WHERE email = :email and password = :password'))
+        {
+            $query->bindParam(':email',$email);
+            $query->bindParam(':password',$password);
+
+            $query->execute();
+
+            // See if the user with these credentials exists
+            if($row = $query->fetch())
+            {
+                session_start();
+                $_SESSION['id'] = $row['id'];
+                $_SESSION['user'] = $row['username'];
+                $_SESSION['perm'] = $row['permission_level'];
+            }
+            else
+            {
+                echo "Invalid email or password";
+            }
+        }
+        else
+        {
+            echo "Error querying the database";
+        }
+    }
+
+
+
+
+?>
