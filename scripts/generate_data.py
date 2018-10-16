98a94ab3027e6804c3809e6ce5aab4eda27bedd6
Tiger <tgsachse@gmail.com>
2018-10-16 19:58:30 -0400

    fixed generation script and make some small tweaks

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -8,7 +8,7 @@ import random
 import string
 from faker import Faker
 
-# Constants.
+# Various constants and global objects.
 HEADER = (
     "/*\n"
     "Part of Zajedno.\n"
@@ -17,30 +17,61 @@ HEADER = (
     "USE main_database;\n"
     "CONNECT main_database;\n\n"
 )
-FAKER = Faker()
 FOOTER = "\nQUIT"
-USER_COUNT = 1000
-UNIVERSITY_COUNT = 100
+USER_COUNT = 10
+UNIVERSITY_COUNT = 10
+ENGLISH_FAKER = Faker()
+LOREM_FAKER = Faker("lt_LT")
 OUTPUT_PATH = "add_sample_data.sql"
+PRIVILEGE_LEVELS = ("normal", "admin", "superadmin")
+ROAD_TYPES = ("Boulevard", "Lane", "Court", "Road", "Way", "Street")
+EMAIL_DOMAINS = (
+    "gmail.com",
+    "yahoo.com",
+    "aol.com",
+    "bellsouth.net",
+    "outlook.com",
+    "yahoo.co.uk",
+)
+INSTITUTION_FORMATS = (
+    "{0} University",
+    "University of {0}",
+    "{0} State University",
+    "{0} State College",
+    "{0} Community College",
+)
 
 class User:
     """Class that holds randomized data for a user entry in a database."""
     INSERT = (
         "INSERT INTO users "
-        "(username, password, email, university_id, permission_level) "
-        "VALUES ('{0}', '{1}', '{2}', {3}, '{4}');\n"
+        "(first_name, last_name, password, email, university_id, permission_level) "
+        "VALUES ('{0}', '{1}', '{2}', '{3}', {4}, '{5}');\n"
     )
 
     def __init__(self, university_id):
         """Generate random data for each field for this user."""
-        profile = FAKER.simple_profile()
-
-        self.email = profile["mail"]
+        self.first_name = ENGLISH_FAKER.first_name()
+        self.last_name = ENGLISH_FAKER.last_name()
         self.university_id = university_id
-        self.username = profile["username"]
+        self.email = self.__generate_email()
         self.password = self.__generate_password()
-        self.permission_level = random.choice(("normal", "admin", "superadmin"))
+        self.permission_level = random.choice(PRIVILEGE_LEVELS)
 
+
+    def __generate_email(self):
+        """Generate a random email using this user's name."""
+        email = []
+
+        email.extend(self.first_name)
+        email.append(".")
+        email.append(self.last_name)
+        email.extend(random.choices(string.digits, k=2))
+        email.append("@")
+        email.append(random.choice(EMAIL_DOMAINS))
+
+        return "".join(email)
+    
 
     def __generate_password(self):
         """Generate a randomized password with special characters and digits."""
@@ -66,7 +97,8 @@ class User:
     def __str__(self):
         """Return the SQL insertion command for this user."""
         return self.INSERT.format(
-            self.username,
+            self.first_name,
+            self.last_name,
             self.password,
             self.email,
             self.university_id,
@@ -84,23 +116,29 @@ class University:
 
     def __init__(self):
         """Generate random data for this university's fields."""
+        self.founder = ENGLISH_FAKER.last_name()
         self.name = self.__generate_name()
-        self.address = FAKER.address()
-        self.description = FAKER.sentence()
+        self.address = self.__generate_address()
+        self.description = LOREM_FAKER.paragraph()
 
     
     def __generate_name(self):
         """Generate a random name with a special institutional format."""
-        INSTITUTION_FORMATS = (
-            "{0} University",
-            "University of {0}",
-            "{0} State University",
-            "{0} State College",
-            "{0} Community College",
-        )
+        return random.choice(INSTITUTION_FORMATS).format(self.founder)
 
-        return random.choice(INSTITUTION_FORMATS).format(FAKER.last_name())
 
+    def __generate_address(self):
+        """Generate a random address using this university's founder's name."""
+        address = []
+
+        address.append(ENGLISH_FAKER.building_number())
+        address.append(self.founder)
+        address.append(random.choice(ROAD_TYPES) + ",")
+        address.append(ENGLISH_FAKER.city() + ",")
+        address.append(ENGLISH_FAKER.state_abbr())
+        address.append(ENGLISH_FAKER.postcode())
+
+        return " ".join(address)
 
     def __str__(self):
         """Return the SQL insertion command for this university."""
