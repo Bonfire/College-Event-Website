322371988b283ac9a48ab36593dd1862a3831396
btirumala <barath.tirumala@gmail.com>
2018-11-05 19:21:11 -0500

    Add credits page

---
+++ source/web/credits.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" charset="utf-8">
+
+    <title>College Events - Credits</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous"></script>
+</head>
+<body style="background: url('background.png')">
+<!-- Navbar -->
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand">
+        <span class="ml-2 text-light" style="display: inline-block;">College Events</span>
+    </a>
+    <form class="form-inline">
+        <a href="login.php">
+            <button class="btn btn-outline-warning mr-2 my-sm-0" type="button">Sign In</button>
+        </a>
+        <a href="register.php">
+            <button class="btn btn-warning my-2 my-sm-0" type="button">Register</button>
+        </a>
+    </form>
+</nav>
+
+<!-- Login Form -->
+<div class="container-fluid">
+    <form id="login_form" action="" method="post">
+        <div class="row">
+            <div class="container col-6 col-md-4 card p-3 bg-dark shadow" style="margin-top: 10%;">
+                <span class="mx-auto text-light"><h4>Group 5</h4></span>
+                <hr class="bg-light">
+                <span class="mx-auto text-light">Jonathan Ponader</span>
+                <span class="mx-auto text-light">Baran Barut</span>
+                <span class="mx-auto text-light">Tiger Sachse</span>
+                <span class="mx-auto text-light">Barath Tirumala</span>
+            </div>
+        </div>
+    </form>
+</div>
+</body>
+</html>
